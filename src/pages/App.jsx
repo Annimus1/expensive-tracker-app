3097861f5f88442392d60a5e5bc5e85db27da7d9
pablo <pablo.j98@gmail.com>
2025-01-02 12:18:37 -0400

    create Log in page

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,11 +1,44 @@
+import Login from "./Login"
+
 function App() {
+
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
-  )
+   <Login />
+  );
 }
 
+
+
+/*
+  features
+  debe gestionar varios usuarios 
+  poder ingresar cuanto gano 
+  poder tener deudas 
+  poder tener cuentas por cobrar 
+  que me diga en que gasto mi dinero con un digrama de torta 
+
+  usuario: 
+    nombre
+    numero_telefonico
+    cuenta
+
+  cuenta
+    tipo   {own, deudas, prestamo}: 
+    saldo
+    transaccion[]
+
+  transaccion:
+    cta_origen
+    cta_destino
+    tipo{ingreso, egreso}
+    metodo (efectivo, electronica)
+    fecha
+    monto
+    nota/descripcion
+    categoria
+
+
+
+
+  */ 
 export default App
